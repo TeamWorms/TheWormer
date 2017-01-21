1eececadf6e484f8b1fd3d3853c4ec3682185f59
Handa Zhang <handazh2@gmail.com>
2017-01-21 15:49:02 -0800

    add spring scripts

---
+++ Assets/FollowCubeCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCubeCollider : MonoBehaviour {
+
+    public Transform lookat;
+    public Transform follow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = follow.position;
+        if (lookat == null) return;
+        if(gameObject.tag != "Tail")
+        {
+            transform.forward = -(lookat.transform.position - transform.position).normalized;
+        } else
+        {
+            transform.forward = (lookat.transform.position - transform.position).normalized;
+        }
+
+    }
+}
--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -52,12 +52,12 @@ public class Jump : MonoBehaviour {
 
     bool inputDown()
     {
-        return Input.GetKeyDown(keyCode) || Input.GetButtonDown(controllerInputName);
+        return Input.GetKeyDown(keyCode); //|| Input.GetButtonDown(controllerInputName);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (movementState == MovementState.Ground && onGround && inputDown())
+        if (movementState == MovementState.Ground && GameContext.playerGroundCount > 0 && inputDown())
         {
             if (forceModifyingObject == null || forceModifyingObject.GetComponent<Jump>().onGround)
             {
@@ -89,6 +89,7 @@ public class Jump : MonoBehaviour {
         if (collision.gameObject.tag == "Ground")
         {
             onGround = true;
+            GameContext.playerGroundCount++;
         }
     }
 
@@ -98,6 +99,7 @@ public class Jump : MonoBehaviour {
         if (collision.gameObject.tag == "Ground")
         {
             onGround = false;
+            GameContext.playerGroundCount--;
         }
     }
 
