b1246876b453c795fb0b27e5d2b15d3a922b83bb
Osman Kaan Demiröz <kaandemiroz@gmail.com>
2017-01-22 13:39:41 -0800

    Fixed replay

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour {
 
@@ -27,7 +28,7 @@ public class GameControl : MonoBehaviour {
     [HideInInspector]
     public Jump[] PlayerJoint;
 
-     [HideInInspector]
+    [HideInInspector]
     public GameObject PlayerParent;
 
     [HideInInspector]
@@ -174,31 +175,32 @@ public class GameControl : MonoBehaviour {
     }
     public void Restart()
     {
-        //print(GameContext.BornPos);
         islose = false;
         GameContext.playerGroundCount = 0;
-        /* PlayerParent.gameObject.SetActive(false);
-         for (int i = 0; i < 4; i++)
-         {
-             PlayerJoint[i].gameObject.SetActive(false);
-             PlayerJoint[i].gameObject.transform.localPosition = PlayerJoint[i].originalPos;
-             PlayerJoint[i].gameObject.SetActive(true);
-         }
-         PlayerParent.transform.position = GameContext.BornPos;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        ////print(GameContext.BornPos);
+        ///* PlayerParent.gameObject.SetActive(false);
+        // for (int i = 0; i < 4; i++)
+        // {
+        //     PlayerJoint[i].gameObject.SetActive(false);
+        //     PlayerJoint[i].gameObject.transform.localPosition = PlayerJoint[i].originalPos;
+        //     PlayerJoint[i].gameObject.SetActive(true);
+        // }
+        // PlayerParent.transform.position = GameContext.BornPos;
 
-         PlayerParent.SetActive(true);*/
-        GameObject temp = PlayerParent;
-        GameObject player = Instantiate(playerPrefab);
-        player.transform.position = GameContext.BornPos;
-        PlayerParent = player;
-        PlayerJoint = PlayerParent.GetComponentsInChildren<Jump>();
-        camera.objectToFollow = PlayerJoint[0].transform;
+        // PlayerParent.SetActive(true);*/
+        //GameObject temp = PlayerParent;
+        //GameObject player = Instantiate(playerPrefab);
+        //player.transform.position = GameContext.BornPos;
+        //PlayerParent = player;
+        //PlayerJoint = PlayerParent.GetComponentsInChildren<Jump>();
+        //camera.objectToFollow = PlayerJoint[0].transform;
 
-        Destroy(temp);
-        if (uiController != null)
-        {
-            islose = false;
-            uiController.HideLoseGame();
-        }
+        //Destroy(temp);
+        //if (uiController != null)
+        //{
+        //    islose = false;
+        //    uiController.HideLoseGame();
+        //}
     }
 }
